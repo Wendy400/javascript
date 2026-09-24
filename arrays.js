14378fa29abc2b09afdb81e3c95c54a3d1377749
Wendy400 <wahomewendy@gmail.com>
2026-09-24 16:13:44 +0300

    learning arrays

---
+++ arrays.js
@@ -0,0 +1,8 @@
+const students = []
+students.push("Alice" , "Bob", "Charlie")
+students[1] ="Dan" //changes Bob to Dan
+//students.pop()  deletes the last item on the array
+students.splice(2)//deletes from any index in the array
+console.log (students )
+//console.log(students[1]) output Bob
+
